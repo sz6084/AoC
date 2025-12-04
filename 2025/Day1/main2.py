48e433780e7a86d11eabfb8fb793b57b7aa8565d
sz6084 <101011086+sz6084@users.noreply.github.com>
2025-12-04 12:08:49 -0800

    Finished Day 1 Part 2; main and main2 work. main3 does not

--- 2025/Day1/main2.py
+++ 2025/Day1/main2.py
@@ -1,6 +1,7 @@
 import pathlib
 
-line = pathlib.Path("input.txt").read_text().split()
+base_path = pathlib.Path(__file__).parent.resolve()
+line = (base_path / "input.txt").read_text().split("\n")
 
 num=50 #init
 op=1
@@ -11,14 +12,20 @@ for char in line:
         op=-1
     else:
         op=1
-    num+=(op*int(char[1:]))
+    extractedNum=int(char[1:])
+    num+=(op*extractedNum)
     while num<0:
+        if num+extractedNum!=0:
+            passwd+=1
         num+=100
     while num>=100:
+        if num!=100:
+            passwd+=1
         num-=100
     if num==0:
         passwd+=1
-    #print(num)
+    print("num:",num)
+    print("numZeroes:",passwd)
 print(passwd)
 #452 is too low
 #955 too low
--- 2025/Day1/main3.py
+++ 2025/Day1/main3.py
@@ -7,27 +7,17 @@ lines = (base_path / "input.txt").read_text().split("\n")
 num=50 #init
 op=-1
 passwd=0
-# for line in lines:
-#     if line[0]=='L':
-#         num-=int(line[1:])
-#     else:
-#         num+=int(line[1:])
-#     while num<0:
-#         num+=100
-#     while num>=100:
-#         num-=100
-#     if num==0:
-#         passwd+=1
-# print(passwd)
 
 ## PART TWO ##
 
 for line in lines:
-    extractedNum=int(line[1:])
-    if line[0]=='L':
-        num-=extractedNum
+    extractedNum=int(line[1:]) # goofy ver
+    if line[0]=='L':    
+        num-=extractedNum%100
+        passwd+=int(extractedNum/100)
     else:
-        num+=extractedNum
+        num+=extractedNum%100
+        passwd+=int(extractedNum/100)
     while num<0:
         if num+extractedNum!=0:
             passwd+=1
@@ -40,5 +30,5 @@ for line in lines:
         passwd+=1
     print("num:",num)
     print("numZeroes:",passwd)
-print(passwd)
+print("numZeroes:",passwd)
 #6863 too high
