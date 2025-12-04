0fc1a55cbc43d8e862939372e40f39ffa0d20cdb
sz6084 <101011086+sz6084@users.noreply.github.com>
2025-12-04 12:25:42 -0800

    removed unnecessary lines, commented out unneeded prints, and adjusted formatting

--- 2025/Day1/main2.py
+++ 2025/Day1/main2.py
@@ -6,7 +6,6 @@ line = (base_path / "input.txt").read_text().split("\n")
 num=50 #init
 op=1
 passwd=0
-#print(line)
 for char in line:
     if char.startswith("L"):
         op=-1
@@ -24,8 +23,8 @@ for char in line:
         num-=100
     if num==0:
         passwd+=1
-    print("num:",num)
-    print("numZeroes:",passwd)
+    #print("num:",num)
+    #print("numZeroes:",passwd)
 print(passwd)
 #452 is too low
 #955 too low
